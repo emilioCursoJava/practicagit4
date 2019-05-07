c060aaf504e5e0e66d19c9729e918e627ba19861
Goferello <48448021+Goferello@users.noreply.github.com>
2019-05-07 19:43:34 +0200

    Cambios en los métodos

--- practicaGit4/src/es/vass/practica/CrearAtributos.java
+++ practicaGit4/src/es/vass/practica/CrearAtributos.java
@@ -2,66 +2,39 @@ package es.vass.practica;
 
 public class CrearAtributos {
 
-	public String atributoDeEmilio ="Emilio";
+	public String atributoDeEmilio = "Emilio";
 	public String crearAtributosAqui = "listaAtributos";
-	
-	
 
 	public String crearAtributosAquiDavidJose = "listaAtributosDavidJose--OK--Vale--Vale";
 
-
-
-
-	
-	
-	public void metodoEmilioModificarJuan () {
+	public void metodoEmilioModificarJuan() {
 		System.out.println("Juan ---");
 	}
 
-
-	public void metodoEmilioModificarHansel () {
+	public void metodoEmilioModificarHansel() {
 		System.out.println("Hansel --- ok");
 	}
 
-	public void metodoEmilioModificarEdgar () {
+	public void metodoEmilioModificarEdgar() {
 		System.out.println("Edgar --- Ok");
 	}
 
-	
-	public void metodoEmilioModificarIvan () {
+	public void metodoEmilioModificarIvan() {
 		System.out.println("Ivan --- OK_nueva Rama");
 	}
 
-
-	public void metodoEmilioModificarJose () {
-		System.out.println("Jose --- Ok");
+	public void metodoEmilioModificarDavid() {
+		System.out.println("David --- OK_Modificado");
 	}
 
-	public void metodoEmilioModificarDavid () {
-		System.out.println("David --- OK");
+	public void metodoEmilioModificarJose() {
+		System.out.println("Jose --- OK_Modificado");
 	}
 
+	public String crearAtributosAquiEdgarIvan2 = "listaAtributosEdgarIvan";
 
-	public String crearAtributosAquiEdgarIvan2= "listaAtributosEdgarIvan";
-
-
-	
-
-	public String crearAtributosAquiHanselJuan2= "listaAtributosHanselJuan";
-	
-	
-	
-	
+	public String crearAtributosAquiHanselJuan2 = "listaAtributosHanselJuan";
 
 	public String crearAtributosAquiDavidJose2 = "listaAtributosDavidJose";
 
-
-	
-	
-
-
-	
-
-	
-	
 }
