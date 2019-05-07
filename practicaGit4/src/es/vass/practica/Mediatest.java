db880e0124b500838a0f4e3b9651ab848d8c87bf
Emilio <emilio89@gmail.com>
2019-05-07 18:37:32 +0200

    first commit

---
+++ practicaGit4/src/es/vass/practica/Mediatest.java
@@ -0,0 +1,64 @@
+package es.vass.practica;
+
+
+import java.net.MalformedURLException;
+
+import java.net.URL;
+
+import javax.swing.JFileChooser;
+
+import javax.swing.JFrame;
+
+public class Mediatest {
+
+public static void main (String[] args) {
+
+JFileChooser filechoser=new JFileChooser();
+
+int resul=filechoser.showOpenDialog(null);
+
+if(resul == JFileChooser.APPROVE_OPTION){
+
+URL mediaURL=null;
+
+try{
+ mediaURL=filechoser.getSelectedFile().toURL();
+
+} catch(MalformedURLException malforme){
+
+System.err.println("Error no hay url");
+
+}
+
+if(mediaURL != null){
+
+JFrame mediaTest=new JFrame("Media");
+
+mediaTest.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+Reproductor repo=new Reproductor(mediaURL);
+
+mediaTest.add(repo);
+
+mediaTest.setSize(300,300);
+
+mediaTest.setVisible(true); }
+
+}
+
+}
+
+public void metodoEmilioModificarIvan () {
+	System.out.println("Ivan ---");
+}
+
+
+public void metodoEmilioModificarJose () {
+	System.out.println("Jose ---");
+}
+
+public void metodoEmilioModificarDavid () {
+	System.out.println("David ---");
+}
+
+}
---
+++ practicaGit4/src/es/vass/practica/Reproductor.java
@@ -0,0 +1,72 @@
+package es.vass.practica;
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.io.IOException;
+import java.net.URL;
+
+import javax.media.*;
+import javax.swing.JPanel;
+
+public class Reproductor extends JPanel
+{
+
+public Reproductor(URL mediaURL) 
+{
+
+	setLayout(new BorderLayout());
+	
+	Manager.setHint(Manager.LIGHTWEIGHT_RENDERER,true);
+	
+	try{
+	
+		Player mediaPlayer=Manager.createRealizedPlayer(mediaURL);
+		
+		Component video= mediaPlayer.getVisualComponent(); 
+		Component controls=mediaPlayer.getControlPanelComponent();
+		
+		if(video!= null)
+		{
+			 add(video,BorderLayout.CENTER); 
+		}
+		
+		if(controls!=null)
+		{
+		 add(controls,BorderLayout.SOUTH); }
+		
+		mediaPlayer.start();
+	
+		}
+	catch(NoPlayerException noplayerexception)
+	{
+	
+		System.out.println("no hay archivo"); 
+	}
+	
+	catch(CannotRealizeException noreleace)
+	{
+	
+		System.out.println("archivo desconocido");
+	 }
+	
+	catch(IOException es)
+	{
+	
+		System.out.println("Error al leer la fuente");
+	}
+}
+
+public void metodoEmilioModificarJuan () {
+	System.out.println("Juan ---");
+}
+
+
+public void metodoEmilioModificarHansel () {
+	System.out.println("Hansel ---");
+}
+
+public void metodoEmilioModificarEdgar () {
+	System.out.println("Edgar ---");
+}
+
+
+}
